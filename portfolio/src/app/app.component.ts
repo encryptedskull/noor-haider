cb714aa042ea8a84d8f9ef5c21ab232407a10bac
Dr. Zero <162797786+encryptedskull@users.noreply.github.com>
2025-07-28 15:26:01 +0500

    Revert "added ui library"
    This reverts commit a4e31798bb925cbbec25b625cbc934e7641b8742.

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatButtonModule, MatCardModule],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'portfolio';
